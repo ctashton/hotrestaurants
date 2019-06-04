4721e5bfeb72d6a269327cf0b712037044aee870
kidffiti <coreyash10@gmail.com>
2019-06-04 19:50:28 -0400

    data file added

---
+++ data/data.js
@@ -0,0 +1,17 @@
+var reservations = [
+    {
+        id:1,
+        name: "Corey",
+        email: "coreysemail@email.com",
+        phone: "4075555500"
+    }
+]
+
+var waitlist = [
+    {
+        id:2,
+        name: "John",
+        email: "johnsemail@email.com",
+        phone: "4075555500"
+    }
+]
