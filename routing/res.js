cc93a67046f05011a88ce74bd57f2026ef7f5c60
Rubentba <rubenatorresbeltran@gmail.com>
2019-06-04 20:20:30 -0400

    add apis

--- routing/res.js
+++ routing/res.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var path = require("path");
+var data = require("./data/data.js")
 
 router.use(function timelog(req, res, next){
     console.log("time: " , Date.now())
@@ -9,10 +10,16 @@ router.use(function timelog(req, res, next){
 
 router.get("api/res", function(req, res) {
 
+    return res.json(data.reservations)
 })
 
 router.get("api/waitlist", function(req, res) {
-    
+    return res.json(waitlist)
 })
+
+router.post("/api/res", newTable)
+.then(function(data) {
+  console.log("make.html", data.reservation);
+});
 
 module.exports = router;
